b803991b5955e496f3f659a0fa3c9eec118f9268
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-11-06 17:45:11 +0000

    Chocolate service save method complete

--- chocolate_start_point/src/main/java/com/bnta/chocolate/services/ChocolateService.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/services/ChocolateService.java
@@ -1,4 +1,25 @@
 package com.bnta.chocolate.services;
 
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ChocolateService {
+    @Autowired
+    ChocolateRepository chocolateRepository;
+
+    public Chocolate saveChocolate(Chocolate chocolate){
+        chocolateRepository.save(chocolate);
+        return chocolate;
+    }
+
+    public Optional<Chocolate> getChocolateById(long id){
+        return chocolateRepository.findById(id);
+    }
+
 }
