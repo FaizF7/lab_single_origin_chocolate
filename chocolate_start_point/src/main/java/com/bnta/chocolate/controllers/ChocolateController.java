6ac8f3dfc175aeb5def409bab511dbbd35489512
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-11-06 18:50:22 +0000

    Added ResponseEntity and HttpStatus codes. MVP.

--- chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -3,6 +3,8 @@ package com.bnta.chocolate.controllers;
 import com.bnta.chocolate.models.Chocolate;
 import com.bnta.chocolate.services.ChocolateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,7 @@ public class ChocolateController {
     ChocolateService chocolateService;
 
     @GetMapping
-    public List<Chocolate> getAllChocolates(){
-        return chocolateService.getAllChocolates();
+    public ResponseEntity<List<Chocolate>> getAllChocolates(){
+        return new ResponseEntity<>(chocolateService.getAllChocolates(), HttpStatus.OK);
     }
 }
--- chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/EstateController.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/EstateController.java
@@ -3,6 +3,8 @@ package com.bnta.chocolate.controllers;
 import com.bnta.chocolate.models.Estate;
 import com.bnta.chocolate.services.EstateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,7 @@ public class EstateController {
     EstateService estateService;
 
     @GetMapping
-    public List<Estate> getAllEstates(){
-        return estateService.getAllEstates();
+    public ResponseEntity<List<Estate>> getAllEstates(){
+        return new ResponseEntity<>(estateService.getAllEstates(), HttpStatus.OK);
     }
 }
